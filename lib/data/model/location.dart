867c6484c3823153686a072a4d42657bfd37ab9e
ilsong963 <ilsong963@naver.com>
2025-04-16 20:36:33 +0900

    feat: tojson fromjson 추가

--- lib/data/model/location.dart
+++ lib/data/model/location.dart
@@ -1,11 +1,27 @@
-class Location {
+class Welcome {
   String lastBuildDate;
   int total;
   int start;
   int display;
   List<Item> items;
 
-  Location({required this.lastBuildDate, required this.total, required this.start, required this.display, required this.items});
+  Welcome({required this.lastBuildDate, required this.total, required this.start, required this.display, required this.items});
+
+  factory Welcome.fromJson(Map<String, dynamic> json) => Welcome(
+    lastBuildDate: json["lastBuildDate"],
+    total: json["total"],
+    start: json["start"],
+    display: json["display"],
+    items: List<Item>.from(json["items"].map((x) => Item.fromJson(x))),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "lastBuildDate": lastBuildDate,
+    "total": total,
+    "start": start,
+    "display": display,
+    "items": List<dynamic>.from(items.map((x) => x.toJson())),
+  };
 }
 
 class Item {
@@ -30,4 +46,28 @@ class Item {
     required this.mapx,
     required this.mapy,
   });
+
+  factory Item.fromJson(Map<String, dynamic> json) => Item(
+    title: json["title"],
+    link: json["link"],
+    category: json["category"],
+    description: json["description"],
+    telephone: json["telephone"],
+    address: json["address"],
+    roadAddress: json["roadAddress"],
+    mapx: json["mapx"],
+    mapy: json["mapy"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "title": title,
+    "link": link,
+    "category": category,
+    "description": description,
+    "telephone": telephone,
+    "address": address,
+    "roadAddress": roadAddress,
+    "mapx": mapx,
+    "mapy": mapy,
+  };
 }
