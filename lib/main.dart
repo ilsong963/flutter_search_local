598dd5059c5a810907e01d532eaf82cb0354521d
ilsong963 <ilsong963@naver.com>
2025-04-18 19:14:11 +0900

    feat: init naver map

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_search_local/presentation/page/home/home_page.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized(); // 1번코드
-  await dotenv.load(fileName: ".env"); // 2번코드
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+
+  final String xNcpApigwApiKeyId = dotenv.get("X_NCP_APIGW_API_KEY_ID");
+  await FlutterNaverMap().init(clientId: xNcpApigwApiKeyId);
 
   runApp(const MainApp());
 }
