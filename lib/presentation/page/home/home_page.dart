0b0eae615cca50cc5d87170cf39050429baa083f
ilsong963 <ilsong963@naver.com>
2025-04-18 13:01:22 +0900

    feat: bottom sheet 추가

--- lib/presentation/page/home/home_page.dart
+++ lib/presentation/page/home/home_page.dart
@@ -35,13 +35,57 @@ class HomePage extends ConsumerWidget {
           ],
         ),
       ),
-      body: ListView.builder(
-        padding: EdgeInsets.all(10),
-        itemCount: searchLocalResultList?.length ?? 0,
-
-        itemBuilder: (context, index) {
-          return Padding(padding: EdgeInsets.all(5), child: LocalSearchResultCard(result: searchLocalResultList![index]));
-        },
+      body: Stack(
+        children: [
+          DraggableScrollableSheet(
+            initialChildSize: 0.05, // 초기 높이를 5%로 설정
+            minChildSize: 0.05, // 최소 높이를 5%로 설정
+            maxChildSize: 0.8, // 최대 높이를 80%로 설정
+            builder: (context, scrollController) {
+              return Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black26,
+                      blurRadius: 10,
+                      spreadRadius: 5,
+                      offset: Offset(0, -5), // 그림자의 위치 조정
+                    ),
+                  ],
+                ),
+                child: Stack(
+                  children: [
+                    // 손잡이 역할을 하는 위젯
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(top: 16),
+                          width: 50,
+                          height: 5,
+                          decoration: BoxDecoration(color: Colors.grey[400], borderRadius: BorderRadius.circular(10)),
+                        ),
+                      ],
+                    ),
+                    Positioned.fill(
+                      child: ListView.builder(
+                        padding: EdgeInsets.all(30),
+                        controller: scrollController,
+                        itemCount: searchLocalResultList?.length ?? 0,
+                        physics: ClampingScrollPhysics(),
+                        itemBuilder: (context, index) {
+                          return Padding(padding: EdgeInsets.all(5), child: LocalSearchResultCard(result: searchLocalResultList![index]));
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+        ],
       ),
     );
   }
